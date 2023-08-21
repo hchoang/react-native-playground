97b4b517aab54859668c84e7f2cff8957c937f2d
Huy Cat Hoang <cat.hoanghuy@gmail.com>
2023-08-21 11:53:55 +1000

    updated main page

--- App.js
+++ App.js
@@ -1,20 +1,29 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { NavigationContainer } from "@react-navigation/native";
+
+import SeletionPage from "./pages/Auth/SelectionPage";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up abc App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SeletionPage/>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: 8
   },
+  inputContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between'
+  },
+  textInput: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    width: '80%'
+  }
 });
