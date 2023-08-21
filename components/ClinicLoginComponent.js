964ecd6231578cd542d88fdcd1bfcd659c0fac38
Huy Cat Hoang <cat.hoanghuy@gmail.com>
2023-08-21 11:53:42 +1000

    added login component

---
+++ components/ClinicLoginComponent.js
@@ -0,0 +1,11 @@
+import { TextInput } from "react-native";
+
+function ClinicLoginComponent() {
+    return (
+        <View>
+            <TextInput></TextInput>
+        </View>
+    )
+}
+
+export default ClinicLoginComponent;
